1ab20642fe29e5b8f7ec6e75930a27f71c66385b
mb83pl <mbutora@gmail.com>
2022-10-18 12:46:11 +0200

    ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import {HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-  HttpClientModule],
+  HttpClientModule,
+  NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
