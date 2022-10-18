96c57ee5d0b70e8ecaae29f7961c397dca424b3f
mb83pl <mbutora@gmail.com>
2022-10-18 13:44:05 +0200

    Title

--- src/app/by-date/by-date.component.ts
+++ src/app/by-date/by-date.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NasaApi} from "../model/nasa-api";
 import {NasaApiService} from "../nasa-api.service";
 import {ActivatedRoute} from "@angular/router";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-by-date',
@@ -13,13 +14,13 @@ export class ByDateComponent implements OnInit {
   dataFromNasaByDate: NasaApi[] = []
   givenDate!: string
 
-  constructor(private service: NasaApiService, private route: ActivatedRoute) { }
+  constructor(private service: NasaApiService, private route: ActivatedRoute, private title: Title) { }
 
   ngOnInit(): void {
     this.givenDate = this.route.snapshot.params['date'];
     this.service.getDataFromNasaByDate(this.givenDate).subscribe(data => {
       this.dataFromNasaByDate = data;
-      console.log("data", data);
+      this.title.setTitle(`NASA - (${this.givenDate}) Picture Of The Day`)
     })
   }
 
